a61a66bd56fff42d902658750be5a75fa3c1fae9
Vyacheslav Kochergin <141725581+VyacheslavIurevich@users.noreply.github.com>
2024-07-20 12:58:38 +0300

    Fix: addition of pragma once (#6)
    * fix: addition of pragma once
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * revert: revert last changes
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    * fix: remove static
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
    
    ---------
    
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- src/calculator/calculator.h
+++ src/calculator/calculator.h
@@ -5,21 +5,23 @@
   9 // my numerical system base is 10^9, if you change it nothing will work
 #define TEN_POWER_BASE 1000000000
 
-bool is_bigger(dllist_t *first_num, dllist_t *second_num, uint8_t *exit_code);
+bool is_bigger(dllist_t *first_num, dllist_t *second_num,
+                      uint8_t *exit_code);
 
 dllist_t *zero(uint8_t *exit_code);
 
-dllist_t *addition(dllist_t *first_num, dllist_t *second_num, bool negative,
-                   uint8_t *exit_code);
+dllist_t *addition(dllist_t *first_num, dllist_t *second_num,
+                          bool negative, uint8_t *exit_code);
 
-dllist_t *subtraction(dllist_t *first_num, dllist_t *second_num, bool negative,
-                      uint8_t *exit_code);
+dllist_t *subtraction(dllist_t *first_num, dllist_t *second_num,
+                             bool negative, uint8_t *exit_code);
 
-dllist_t *multiplication(dllist_t *first_num, dllist_t *second_num, int8_t sign,
-                         uint8_t *exit_code);
+dllist_t *multiplication(dllist_t *first_num, dllist_t *second_num,
+                                int8_t sign, uint8_t *exit_code);
 
-dllist_t *calculate_operation_result(dllist_t *first_num, dllist_t *second_num,
-                                     char operation, uint8_t *exit_code);
+dllist_t *calculate_operation_result(dllist_t *first_num,
+                                            dllist_t *second_num,
+                                            char operation, uint8_t *exit_code);
 
 bool isoperation(char ch);
 
